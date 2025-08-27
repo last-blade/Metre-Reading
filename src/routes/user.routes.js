3550cef12f4ccf5694b6cd97dba7e88e5e795ae5
last-blade <meprashanttyagi2000@gmail.com>
2025-08-27 17:02:35 +0530

    add auth middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { registerUser } from "../controllers/userControllers/registerUser.controller.js";
 import { loginUser } from "../controllers/userControllers/loginUser.controller.js";
 import { logoutUser } from "../controllers/userControllers/logoutUser.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 //POST
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").post(logoutUser);
+router.route("/logout").post(authMiddleware, logoutUser);
 
 export default router;
